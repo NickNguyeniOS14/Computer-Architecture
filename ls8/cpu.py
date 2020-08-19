4d9d9c2f81a7f9e5438bf36346da8674a74a50f7
Nick <ptnguyen1901@gmail.com>
2020-08-18 21:33:15 -0400

    MVP day 2.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,85 +3,71 @@
 import sys
 
 
+LDI = 0b10000010
+PRN = 0b01000111 # Print
+HLT = 0b00000001  # Halt
+MUL = 0b10100010  # Multiply
+ADD = 0b10100000  # Addition
+SUB = 0b10100001 # Subtraction
+DIV = 0b10100011 # Division
+
+
 class CPU:
     """Main CPU class."""
-
-    ops = {
-        0x00: "NOP",  # no-op
-        0x01: "HLT",  # halt; exit
-        0x50: "CALL",  # call subroutine at register address
-        0x11: "RET",  # return from subroutine
-        0x52: "INT",  # interrupt
-        0x13: "IRET",  # return from interrupt
-        0x54: "JMP",  # jump to address in given reg
-        0x55: "JEQ",  # if == flag true, jump to address in given reg
-        0x56: "JNE",  # if E flag is clear, jump to address in given reg
-        0x57: "JGT",  # if >, jump to address in given reg
-        0x58: "JLT",  # if <
-        0x59: "JLE",  # if < or ==
-        0x5A: "JGE",  # if > or ==
-        0x82: "LDI",  # set value of register to integer
-        0x83: "LD",  # loads regA w/ value at mem address stored in regB
-        0x84: "ST",  # store value in regB at address in regA
-        0x45: "PUSH",  # push value in given reg onto stack
-        0x46: "POP",  # pop value at top of stack into given reg
-        0x47: "PRN",  # print numeric value stored in given reg
-        0x48: "PRA",  # print alpha char value stored in given reg
-        0xA0: "ADD",  # addition (ALU)
-        0xA1: "SUB",  # subtract (ALU)
-        0xA2: "MUL",  # multiply (ALU)
-        0xA3: "DIV",  # division (ALU)
-        0xA4: "MOD",  # modulo (ALU)
-        0x65: "INC",  # increment (ALU)
-        0x66: "DEC",  # decrement (ALU)
-        0xA7: "CMP",  # comparison (ALU)
-        0xA8: "AND",  # bitwise-and (ALU)
-        0x69: "NOT",  # bitwise-not (ALU)
-        0xAA: "OR",  # bitwise-or (ALU)
-        0xAB: "XOR",  # XOR (ALU)
-        0xAC: "SHL",  # bitshift left (ALU)
-        0xAD: "SHR",  # bitshift right (ALU)
-    }
-
 
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256
-        self.cpu_reg = [0] * 8
+        self.reg = [0] * 8
+        self.pc = 0
+        self.running = True
+        self.reg[7] = 0xF4
 
-    def ram_read(self, address):
-        return self.ram[address]
-
-    def ram_write(self, value, address):
-        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
+        filename = sys.argv[1]
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        #    ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        with open(filename) as f:
+            for line in f:
+                line = line.split("#")[0].strip()
+                if line == "":
+                    continue
+                else:
+                    self.ram[address] = int(line, 2)
+                    address += 1
+
+
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "DIV":
+            self.reg[reg_a] /= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -93,8 +79,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            # self.fl,
-            # self.ie,
+            #self.fl,
+            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -105,23 +91,49 @@ class CPU:
 
         print()
 
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
+
+    def HLT(self):
+        self.running = False
+
     def run(self):
-        ir = self.ram_read(self.pc)  # instruction register
+        """Run the CPU."""
+        self.load()
+        while self.running:
+            instruction_register = self.ram[self.pc]
+            reg_a = self.ram[self.pc + 1]
+            reg_b = self.ram[self.pc + 2]
 
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
-        operation = CPU.ops.get(ir)
-
-        if operation is None:
-            return
-        elif operation == "HLT":
-            return
-        elif operation == "LDI":
-            self.reg[operand_a] = operand_b
-        elif operation == "PRN":
-            print(self.reg[operand_a])
-
-        self.pc += (ir >> 6) + 1
-        self.run()
-
+            if instruction_register == HLT:
+                self.running = False
+                self.pc += 1
+            elif instruction_register == LDI:
+                self.reg[reg_a] = reg_b
+                self.pc += 3
+            elif instruction_register == PRN:
+                print(self.reg[reg_a])
+                self.pc += 2
+            elif instruction_register == MUL:
+                self.reg[reg_a] *= self.reg[reg_b]
+                self.pc += 3
+            elif instruction_register == PUSH:
+                self.reg[SP] -= 1
+                stack_address = self.reg[SP]
+                register_number = self.ram_read(self.pc + 1)
+                register_number_value = self.reg[register_number]
+                self.ram_write(stack_address, register_number_value)
+                self.pc += 2
+            elif instruction_register == POP:
+                stack_value = self.ram_read(self.reg[SP])
+                register_number = self.ram_read(self.pc + 1)
+                self.reg[register_number] = stack_value
+                self.reg[SP] += 1
+                self.pc += 2
+            else:
+                print(f"Instruction '{instruction_register}'' at address '{self.pc}' is not recognized")
+                self.pc += 1
 
